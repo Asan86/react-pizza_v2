208455ae18a61dfc051cbde4254d563974803a50
AsanTurdukulov <asanturdukulov86@gmail.com>
2023-07-01 22:41:52 +0600

    react-pizza_v2--lesson-4

---
+++ src/components/Categories.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = [
+    "Вегетарианская",
+    "Гриль",
+    "Мясные",
+    "Все",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li
+            onClick={() => onClickCategory(i)}
+            className={activeIndex === i ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
